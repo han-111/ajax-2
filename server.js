f770b65573fe1a68f5d65470c91569804d797107
hanxin <xin_hero111@163.com>
2021-07-25 09:31:27 +0800

    静态服务器

--- server.js
+++ server.js
@@ -20,8 +20,6 @@ var server = http.createServer(function (request, response) {
   /******** 从这里开始看，上面不要看 ************/
 
   console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
-
-  if (path === '/') {
     response.statusCode = 200
     const requestPath = path==='/'?'index.html':path
     console.log(requestPath)
@@ -43,13 +41,6 @@ var server = http.createServer(function (request, response) {
     }
     response.write(content)
     response.end()
-  } else {
-    response.statusCode = 404
-    response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`你输入的路径不存在对应的内容`)
-    response.end()
-  }
-
   /******** 代码结束，下面不要看 ************/
 })
 
